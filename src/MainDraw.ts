d4bf5ca9752c9569c7d0e131f6473bf0b036b134
posemiya <posemiya@gmail.com>
2016-03-21 17:14:08 +0900

    キャラ表示

--- src/MainDraw.ts
+++ src/MainDraw.ts
@@ -2,14 +2,20 @@
 
 class MainDraw {
     private imageMap;
+    private imageChara = [];
 
     constructor() {
         this.imageMap = new Image();
         this.imageMap.src = "image/map/map1.png";
+        for (var n = 0; n < 8; n++) {
+            this.imageChara[n] = new Image();
+            this.imageChara[n].src = "image/chara/chara1_"+n+".png";
+        }
     }
 
     private draw(context: any): void {
         this.drawMap(context);
+        this.drawMyself(context);
     }
 
     private drawMap(context: any): void {
@@ -21,5 +27,12 @@ class MainDraw {
                     y * this.imageMap.height);
             }
         }
+    }
+
+    private drawMyself(context: any): void {
+        context.drawImage(
+            this.imageChara[0],
+            1024 / 2 - this.imageChara[0].width / 2,
+            600 / 2 - this.imageChara[0].height / 2);
     }
 }
