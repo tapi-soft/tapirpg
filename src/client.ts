063708e047d00ad6dc232b9a50d4f7bd4fbb29f5
posemiya <posemiya@gmail.com>
2016-03-21 14:17:05 +0900

    マップタイル描画

--- src/client.ts
+++ src/client.ts
@@ -4,8 +4,27 @@ $(function() {
     var canvas = <HTMLCanvasElement>document.getElementById("canvas");
     var context = canvas.getContext("2d");
     var socket = io.connect();
+    var imageMap;
 
     window.onload = function() {
+        init();
+        setInterval(function() {
+            draw();
+        }, 16);
+    }
 
+    function init() {
+        imageMap = new Image();
+        imageMap.src = "image/map/map1.png";
+    }
+
+    function draw() {
+        context.fillStyle="rgba(255,255,255,1)";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        for (var y = 0; y < 20; y++) {
+            for (var x = 0; x < 20; x++) {
+                context.drawImage(imageMap, x * 64, y * 32);
+            }
+        }
     }
 })
