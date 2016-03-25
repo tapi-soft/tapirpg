1d9a67b42db3c3c7b6cd12d2d066b079d9e4794a
posemiya <posemiya@gmail.com>
2016-03-25 15:58:40 +0900

    フォルダ移動

--- src/common/main.d.ts
+++ src/common/main.d.ts
@@ -1,1 +1,5 @@
 /// <reference path="./Character.ts"/>
+/// <reference path="./EmitCharacterData.ts"/>
+/// <reference path="./EmitPlayerData.ts"/>
+/// <reference path="./EmitMonsterData.ts"/>
+/// <reference path="./EmitData.ts"/>
--- src/main.d.ts
+++ src/main.d.ts
@@ -1,8 +1,4 @@
 /// <reference path="./typings/main.d.ts"/>
 /// <reference path="./common/main.d.ts"/>
 /// <reference path="./client/main.d.ts"/>
-/// <reference path="./EmitCharacterData.ts"/>
-/// <reference path="./EmitPlayerData.ts"/>
-/// <reference path="./EmitMonsterData.ts"/>
-/// <reference path="./EmitData.ts"/>
 /// <reference path="./server/main.d.ts"/>
